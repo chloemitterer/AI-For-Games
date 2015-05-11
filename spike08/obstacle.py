f930af071405ff74ca34c4af7a71f48b2cdcd323
chloemitterer <9725725@student.swin.edu.au>
2015-05-11 18:13:04 +1000

    spike 8 done

---
+++ spike08/obstacle.py
@@ -0,0 +1,15 @@
+from vector2d import Vector2D
+from vector2d import Point2D
+from graphics import egi, KEY
+from math import sin, cos, radians
+from random import random, randrange, uniform
+
+class Obstacle(object):
+
+	def __init__(self, world = None):
+		self.world = world
+		self.pos = Vector2D(randrange(world.cx), randrange(world.cy))
+
+	def render(self, color=None):
+		egi.red_pen()
+		egi.circle(self.pos, 30)
